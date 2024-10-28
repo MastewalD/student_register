c3810b7d3f5c0e9d236d49cb7bbe751299e3b1c5
mastewaldegu <mastewaldegu0@gmail.com>
2024-10-28 11:05:22 -0700

    express server created

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+require("dotenv").config()
+const port = process.env.PORT
+const app = express()
+
+app.listen(port,()=>{
+    console.log(`server run on port ${port}`)
+})
